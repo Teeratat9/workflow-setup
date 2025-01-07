d46d4a2c1427c57ffa1b1bdbf0d9a1d8623cf807
Teeratat Peromtukorn <p.teeratat@hotmail.com>
2025-01-08 00:39:19 +0700

    Fixed prettier_linter

--- nvim/lua/plugins/prettier_linter.lua
+++ nvim/lua/plugins/prettier_linter.lua
@@ -9,15 +9,29 @@ return {
         sources = {
           null_ls.builtins.formatting.prettier.with({
             extra_filetypes = { "xml", "handlebars" }, -- Enable Prettier for XML and .hbs
+            condition = function(utils)
+              -- Check if Prettier configuration exists in the project
+              return utils.root_has_file({
+                ".prettierrc",
+                ".prettierrc.json",
+                ".prettierrc.js",
+                ".prettierrc.yml",
+                ".prettierrc.yaml",
+                "prettier.config.js",
+                "prettier.config.cjs",
+                ".prettierrc.toml",
+              })
+            end,
           }),
           null_ls.builtins.diagnostics.eslint, -- ESLint diagnostics
           null_ls.builtins.formatting.eslint, -- ESLint formatting (optional)
         },
       })
 
-      -- Auto-format on save for additional filetypes including .hbs and .xml
+      -- Auto-format and auto-save on save
       vim.cmd([[
         autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.html,*.hbs,*.xml lua vim.lsp.buf.format()
+        autocmd BufWritePost *.js,*.jsx,*.ts,*.tsx,*.json,*.css,*.scss,*.html,*.hbs,*.xml silent! update
       ]])
 
       -- Set .hbs files to be recognized as HTML for syntax highlighting
@@ -49,12 +63,12 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = { "html", "javascript", "typescript", "css", "xml", "handlebars" }, -- Add XML and Handlebars
-        sync_install = false, -- ไม่ sync ติดตั้ง parser
-        ignore_install = {}, -- ไม่ละเว้นการติดตั้งภาษาใดๆ       auto_install = true, -- ติดตั้ง parser อัตโนมัติเมื่อเปิดไฟล์
+        sync_install = false, -- Don't sync-install parsers
+        ignore_install = {}, -- Don't ignore any languages
+        auto_install = true, -- Automatically install parsers
         highlight = {
-          enable = true, -- เปิดใช้งาน syntax highlighting
+          enable = true, -- Enable syntax highlighting
         },
-        modules = {},
       })
     end,
   },
